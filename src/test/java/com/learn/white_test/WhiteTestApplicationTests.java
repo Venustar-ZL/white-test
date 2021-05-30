88e4dae301f62cd0752aa03c829e1dcaedcda54f
Venustar-ZL <1577495441@qq.com>
2021-05-30 22:28:00 +0800

    func:init

---
+++ src/test/java/com/learn/white_test/WhiteTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.white_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiteTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
